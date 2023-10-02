688bbe116607076c0c5de3152e6ab542eb32d7d1
Adam G <25728719+Electromaster232@users.noreply.github.com>
2023-10-02 13:51:59 -0400

    Update process.js

--- process.js
+++ process.js
@@ -1,17 +1,16 @@
 /**
  * Welcome to ICAL.js fiddle
  */
- var iCalendarData = ""
   $.ajax({
-    url:'https://campusgroups.rit.edu/ical/rit/ical_rit.ics',
+    url:'http://megaoof.djelectro.me/rit/ical_rit.ics',
     success: function (data){
-      iCalendarData = data
+      var iCalendarData = data
+     
+      var jcalData = ICAL.parse(iCalendarData);
+      var vcalendar = new ICAL.Component(jcalData);
+      var vevent = vcalendar.getFirstSubcomponent('vevent');
+      var summary = vevent.getFirstPropertyValue('summary');
+      console.log('Summary: ' + summary);
     }
   });
 
-
-var jcalData = ICAL.parse(iCalendarData);
-var vcalendar = new ICAL.Component(jcalData);
-var vevent = vcalendar.getFirstSubcomponent('vevent');
-var summary = vevent.getFirstPropertyValue('summary');
-console.log('Summary: ' + summary);
